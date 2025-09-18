82d080416e09c0e800e8e8a209bb0059401eb087
applied-gen-ai <team@appliedgenaicourse.com>
2025-09-18 18:31:42 +0530

    task definition storage

--- Terraform_streamlit_RagApp/Modules/task-definition/task-definition.tf
+++ Terraform_streamlit_RagApp/Modules/task-definition/task-definition.tf
@@ -8,8 +8,8 @@ resource "aws_ecs_task_definition" "app" {
   family                   = "my-app"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "2024"
-  memory                   = "3048"
+  cpu                      = "2048"
+  memory                   = "4096"
   execution_role_arn = var.execution_role_arn
   task_role_arn      = var.task_role_arn
 
