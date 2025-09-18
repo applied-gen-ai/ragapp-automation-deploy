4c2e31b47a616891dce8b30d449518162f6126d2
applied-gen-ai <team@appliedgenaicourse.com>
2025-09-18 18:09:33 +0530

    ecr changes

--- Terraform_streamlit_RagApp/Modules/task-definition/task-definition.tf
+++ Terraform_streamlit_RagApp/Modules/task-definition/task-definition.tf
@@ -8,8 +8,8 @@ resource "aws_ecs_task_definition" "app" {
   family                   = "my-app"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "1024"
-  memory                   = "2048"
+  cpu                      = "2024"
+  memory                   = "3048"
   execution_role_arn = var.execution_role_arn
   task_role_arn      = var.task_role_arn
 
