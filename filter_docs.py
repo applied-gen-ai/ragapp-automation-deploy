0d521778572c65da2d4abc4c49ae2d0493c7dd90
applied-gen-ai <team@appliedgenaicourse.com>
2025-09-19 21:06:42 +0530

    Update filter_docs.py

--- filter_docs.py
+++ filter_docs.py
@@ -1,14 +1,13 @@
 from sentence_transformers import CrossEncoder
-import numpy as np
+import os
 
+HF_TOKEN = os.environ.get("HUGGINGFACE_API", None)
+cross_encoder = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-12-v2", use_auth_token=HF_TOKEN)
 
-cross_encoder = CrossEncoder("/app/model/cross-encoder/ms-marco-MiniLM-L-12-v2")
 def cross_encoder_func(query, documents, k=5):
-
+    import numpy as np
     pairs = [[query, doc.page_content] for doc in documents]
     scores = cross_encoder.predict(pairs)
     top_indices = np.argsort(scores)[::-1][:k]
-
     top_k_documents = [documents[idx] for idx in top_indices]
-
     return scores
